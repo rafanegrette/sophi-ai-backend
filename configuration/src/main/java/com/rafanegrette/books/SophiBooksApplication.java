0a60b0bdae4f60a2af2621232dbab293d03cd6dc
Rafael Negrette <rafaelnegretteamaya@outlook.com>
2023-09-24 13:58:17 -0500

    refactor gradle dependency versions

--- configuration/src/main/java/com/rafanegrette/books/SophiBooksApplication.java
+++ configuration/src/main/java/com/rafanegrette/books/SophiBooksApplication.java
@@ -1,10 +1,5 @@
 package com.rafanegrette.books;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -21,20 +16,5 @@ public class SophiBooksApplication {
 		SpringApplication.run(SophiBooksApplication.class, args);
 		
 	}
-
-	//@EventListener(ApplicationReadyEvent.class)
-	/*public void firstExecution() {
-		byte[] result = azureSpeechService.getBinaryFile("I'm tired of being used");
-		Path path = Path.of("./result.wav");
-		try {
-			Files.deleteIfExists(path);
-			Files.createFile(path);
-			Files.write(path, result);
-		} catch (IOException ex) {
-			log.error("Exception saving file {}", ex.getMessage());
-		}
-		
-
-	}*/
 
 }
