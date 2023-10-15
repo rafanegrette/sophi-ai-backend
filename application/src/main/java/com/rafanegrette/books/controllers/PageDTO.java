91d89b28e3edcaad18555f2b35755b15eb6a0ad6
Rafael Negrette <rafaelnegretteamaya@outlook.com>
2023-10-15 15:33:48 -0500

    delete all service classes

--- application/src/main/java/com/rafanegrette/books/controllers/PageDTO.java
+++
@@ -1,7 +0,0 @@
-package com.rafanegrette.books.controllers;
-
-import com.rafanegrette.books.model.Page;
-
-public record PageDTO(Page page, Integer totalPages) {
-
-}
--- application/src/main/java/com/rafanegrette/books/services/DeleteBookService.java
+++
@@ -1,5 +0,0 @@
-package com.rafanegrette.books.services;
-
-public interface DeleteBookService {
-    void deleteBook(String bookId);
-}
