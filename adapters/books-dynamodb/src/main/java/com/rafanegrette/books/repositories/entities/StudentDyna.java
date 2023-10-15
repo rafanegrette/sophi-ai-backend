ce9a35f6e654bcc532bbfc73d2363af81d675393
Rafael Negrette <rafaelnegretteamaya@outlook.com>
2023-10-15 12:01:07 -0500

    remove all submodules but application

--- adapters/books-dynamodb/src/main/java/com/rafanegrette/books/repositories/entities/StudentDyna.java
+++
@@ -1,9 +0,0 @@
-package com.rafanegrette.books.repositories.entities;
-
-import lombok.Data;
-
-@Data
-public class StudentDyna {
-	private Integer id;
-	private String name;
-}
--- configuration/src/test/java/com/rafanegrette/books/UploadBookAcceptanceTest.java
+++
@@ -1,9 +0,0 @@
-package com.rafanegrette.books;
-
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class UploadBookAcceptanceTest {
-
-}
