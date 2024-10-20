59ed07069c23809e0e334392e0fe361e2bb17e51
Rafael Negrette <rafaelnegretteamaya@outlook.com>
2024-10-20 09:58:32 -0500

    mark the start of a new authentication schema

---
+++ configuration/src/main/java/com/rafanegrette/books/config/CustomOAuth2UserService.java
@@ -0,0 +1,7 @@
+package com.rafanegrette.books.config;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+}
