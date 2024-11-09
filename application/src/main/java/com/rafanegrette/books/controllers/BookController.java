6378f0b00f3a58796a43971b4df567f3207b12f2
Rafael Negrette <rafaelnegretteamaya@outlook.com>
2024-11-09 18:42:29 -0500

    add AWS Cognito oauth2 security option

--- application/src/main/java/com/rafanegrette/books/controllers/BookController.java
+++ application/src/main/java/com/rafanegrette/books/controllers/BookController.java
@@ -3,20 +3,10 @@ package com.rafanegrette.books.controllers;
 import java.io.IOException;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
 import com.rafanegrette.books.model.Book;
 import com.rafanegrette.books.model.Chapter;
 import com.rafanegrette.books.model.Title;
@@ -29,7 +19,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class BookController {
 
-    private Logger LOGGER = LoggerFactory.getLogger(BookController.class);
     private final ReadBookService readBookService;
 
     @GetMapping("/{bookId}")
